dc838b7c151bf55199bb3c11767219a160611827
regevbr <regevbr@gmail.com>
2017-12-02 15:55:53 +0200

    #25 - add support for extracting nested urls in query params

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
+const URL = require('url').Url;
 const urlRegex = require('url-regex');
 const normalizeUrl = require('normalize-url');
-const queryString = require('query-string');
 const unescape = require('unescape');
 
 module.exports = (str, opts) => {
@@ -9,7 +9,7 @@ module.exports = (str, opts) => {
 	const ret = new Set();
 
 	function _extractQueryParams(url) {
-		const qs = queryString.parse(queryString.extract(url));
+		const qs = new URL().parse(url, true).query;
 		for (const key in qs) {
 			if (Object.prototype.hasOwnProperty.call(qs, key) && qs[key].match(urlRegex())) {
 				_add(qs[key]);
@@ -30,4 +30,3 @@ module.exports = (str, opts) => {
 	});
 	return ret;
 };
-
