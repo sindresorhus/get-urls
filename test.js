2a8b846940d3629006673deea606d9968848328d
Omar Diab <od@eco.com>
2019-10-16 16:07:51 +0900

    add test for strict url parsing turned off

--- test.js
+++ test.js
@@ -118,3 +118,15 @@ test('get schemeless url from query string', t => {
 		])
 	);
 });
+
+test('strictUrlParsing turned off', t => {
+	const text = 'Here is a URL: sindresorhus.com here is another: unicorn.education';
+	t.deepEqual(
+		getUrls(text, {
+			strictUrlParsing: false
+		}), new Set([
+			'http://sindresorhus.com',
+			'http://unicorn.education'
+		])
+	);
+});
